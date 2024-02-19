e4d3c235380ab1c3c7d7445b99e097640e7b0186
nescpro <salazarnestor1996@gmail.com>
2024-02-19 17:24:05 -0500

    Fix frontend

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
-app.use(express.static(path.resolve(__dirname, './public')))
+app.use(express.static(path.resolve(__dirname, './client/dist')))
 app.use(cookieParser())
 
 // API routes
@@ -41,7 +41,7 @@ app.use('/api/v1/jobs', authMiddleware, jobsRouter)
 app.use('/api/v1/users', authMiddleware, usersRouter)
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './public', 'index.html'))
+    res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))
 })
 
 
